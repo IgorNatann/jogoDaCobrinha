2610784a1abece9f0b0e7962b837bdd5f670bf6b
Igor Natan <igornatan4@gmail.com>
2022-02-23 13:08:17 -0300

    commit fruta que será consumida

--- script.js
+++ script.js
@@ -9,6 +9,10 @@ snake [0] = {
 }
 
 let direction = "right";
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random () * 15 + 1) * box
+}
 
 
 function criarBG () {
@@ -23,6 +27,11 @@ function criarCobrinha () {
     }
 }
 
+function drawFood () {
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
+}
+
 document.addEventListener('keydown', update);
 
 function update (event) {
@@ -33,7 +42,7 @@ function update (event) {
 }
 
 function iniciarJogo () {
-    
+
     if (snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
     if (snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
     if (snake[0].y > 15 * box && direction == "down") snake[0].y =0;
@@ -41,6 +50,7 @@ function iniciarJogo () {
 
     criarBG();  
     criarCobrinha();
+    drawFood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
